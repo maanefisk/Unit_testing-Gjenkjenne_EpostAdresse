c9feca670ced9d24775f29a8a71d505c74fe443e
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-04-15 23:26:09 +0200

    First test returns true
    test 2 soon to come
    test 3 not started

---
+++ lib.js
@@ -0,0 +1,10 @@
+function checkEmail(txt) {
+        if (txt.indexOf("@")) {
+            return true;
+        }     
+}
+
+function checkSpace(txt) {
+    txt = txt.trim();
+    return txt.substring(0);    
+}
