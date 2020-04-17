1a39668f4097c49bbd657bbc6e42873b85341655
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-04-17 11:19:57 +0200

    Ointment

---
+++ lib2.js
@@ -0,0 +1,15 @@
+function isEmail(txt) {
+    if (txt.includes("@")) return true;
+    
+    
+        let indexOfAt = txt.indexOf("@");
+        let firstDotIndex = txt.indexOf(".");
+        if (firstDotIndex== -1 || firstDotIndex > indexOfAt) return false;
+        
+        let SecondDotIndex = txt.indexOf(".", indexOfAt);
+        return SecondDotIndex >= 0;
+        
+        
+    
+}
+    
